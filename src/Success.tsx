976bb4925d8ad51877f9e022ff66b007ee133971
Lucas Thaw <saiwaizinthaw@gmail.com>
2024-04-25 22:13:42 +0700

    second changes

--- src/Success.tsx
+++ src/Success.tsx
@@ -1,25 +1,35 @@
-import { useNavigate } from "react-router-dom"
-import Complete from "./assets/images/icon-complete.svg"
+import { useNavigate } from "react-router-dom";
+import Complete from "./assets/images/icon-complete.svg";
+import React from "react";
 
 const Success = () => {
   const Navigate = useNavigate();
 
   const btnHandler = () => {
-    Navigate("/"); 
-  }
+    Navigate("/");
+  };
   return (
     <div className="w-8/12 h-screen flex items-center justify-center">
-   <div className="w-8/12 h-screen flex items-center justify-center">
-    <div className="w-[360px] flex items-center justify-center flex-col gap-5">
-      <img src={Complete} alt="Complete Icon" />
-      <span className="text-4xl  text-VeryDarkViolet tracking-widest uppercase font-Space">Thank You!</span>
-      <span className="text-lg font-semibold  text-DarkGrayishViolet tracking-widest font-Space">We've added your card details</span>
+      <div className="w-8/12 h-screen flex items-center justify-center">
+        <div className="w-[360px] flex items-center justify-center flex-col gap-5">
+          <img src={Complete} alt="Complete Icon" />
+          <span className="text-4xl  text-VeryDarkViolet tracking-widest uppercase font-Space">
+            Thank You!
+          </span>
+          <span className="text-lg font-semibold  text-DarkGrayishViolet tracking-widest font-Space">
+            We've added your card details
+          </span>
 
-       <button onClick={btnHandler} className="w-full py-3 m-10 bg-VeryDarkViolet text-white rounded-lg text-lg font-bold active:scale-90 duration-300">Continue</button>
-       </div>
+          <button
+            onClick={btnHandler}
+            className="w-full py-3 m-10 bg-VeryDarkViolet text-white rounded-lg text-lg font-bold active:scale-90 duration-300"
+          >
+            Continue
+          </button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Success
+export default Success;
